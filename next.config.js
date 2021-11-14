31a53f860293c0fd3772254898eb85658acb751c
Bryan Lumbantobing <bryantobing0@gmail.com>
2021-11-14 11:07:16 +0700

    Update deps

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
+    disable: process.env.NODE_ENV === "development",
     dest: "public",
     runtimeCaching,
-    // temporary fix for next@12 https://github.com/shadowwalker/next-pwa/issues/288
-    buildExcludes: [/middleware-manifest\.json$/],
+    scope: "/",
   },
 });
