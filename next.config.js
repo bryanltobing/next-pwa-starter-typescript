0d941a43ee16a3e8c6feaf1207c54cca71f035e4
Bryan Lumbantobing <bryantobing0@gmail.com>
2021-11-14 13:23:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === "development",
     dest: "public",
     runtimeCaching,
-    scope: "/",
+    buildExcludes: [/middleware-manifest\.json$/],
   },
 });
